298167f7af106909bf1b1113c6965c36a074e529
SergoLondaridze <sergo.londaridze@gmail.com>
2023-07-09 20:31:50 +0400

    add visual html and css

---
+++ script.js
@@ -0,0 +1,7 @@
+const circles = document.querySelectorAll(".circle");
+const lines = document.querySelectorAll(".line");
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+
